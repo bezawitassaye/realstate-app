70518e3b0c27d5f76ad25536d8e14a402edffba8
bezawitassaye <beza1221love@gmail.com>
2024-06-17 14:23:10 +0300

    usermodel created

---
+++ api/models/usermodels.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userschema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+      
+    },
+},{timestamps:true})
+
+
+const usermodel = mongoose.models.user ||  mongoose.model("user",userschema)
+export default  usermodel
