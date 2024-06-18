32d70450a659ab2a62d2b7ffa414a53ec7201d27
bezawitassaye <beza1221love@gmail.com>
2024-06-18 19:59:40 +0300

    user login

--- api/routes/userroutes.js
+++ api/routes/userroutes.js
@@ -1,10 +1,10 @@
 import express from "express"
-import { registeruser } from "../controllers/usercontroller.js"
+import { registeruser,loginuser } from "../controllers/usercontroller.js"
 
 const userroute = express.Router()
 
 userroute.post("/register",registeruser)
-
+userroute.post("/login",loginuser)
 
 export default userroute
 
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,37 +1,64 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const SignIn = () => {
-  return (
-    <div className="flex justify-center mt-28 ">
-    <div className="bg-amber-100 p-8 rounded shadow-lg w-full sm:w-96">
-    <h2 className="text-2xl font-semibold mb-4">Sign in</h2>
-    <form>
-      <div className="mb-4">
-        <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
-        <input type="text" id="username" name="username" className="mt-1 block w-full px-3 py-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-        <input type="email" id="email" name="email" autoComplete="email" className="mt-1 block w-full px-3 py-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-        <input type="password" id="password" name="password" autoComplete="current-password" className="mt-1 block w-full px-3 py-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required />
-      </div>
-      <div className="mb-4">
-        <button type="submit" className="w-full bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600">Sign in</button>
-      </div>
-    </form>
-    <div className="text-center">
-      <span className="text-sm text-gray-600">Or sign in with</span>
-      <button className="mt-2 w-full bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600">Login with Google</button>
-    </div>
-    
-        
+const SignOut = () => {
+    const navigate = useNavigate();
+    const [data, setData] = useState({
+        username: "",
+        email: "",
+        password: ""
+    });
 
-   
-  </div>
-  </div>
-  )
+    const changehandler = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setData({ ...data, [name]: value });
+    };
+
+    const onRegister = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post("http://localhost:5019/api/user/login", data);
+            if (response.data.success){
+              navigate("/");
+            }
+           
+            console.log(response.data); // Assuming response.data contains success and message
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <div className="flex justify-center mt-28">
+            <div className="bg-amber-100 p-8 rounded shadow-lg w-full sm:w-96">
+                <h2 className="text-2xl font-semibold mb-4">Sign In</h2>
+                <form onSubmit={onRegister}>
+                    <div className="mb-4">
+                        <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>
+                        <input onChange={changehandler} value={data.username} type="text" id="username" name="username" className="mt-1 block w-full px-3 py-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
+                        <input type="email" id="email" name="email" onChange={changehandler} value={data.email} autoComplete="email" className="mt-1 block w-full px-3 py-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+                        <input onChange={changehandler} value={data.password} type="password" id="password" name="password" autoComplete="current-password" className="mt-1 block w-full px-3 py-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required />
+                    </div>
+                    <div className="mb-4">
+                        <button type="submit" className="w-full bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600">Sign in</button>
+                    </div>
+                </form>
+                <div className="text-center">
+                    <span className="text-sm text-gray-600">Or sign in with</span>
+                    <button className="mt-2 w-full bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600">Login with Google</button>
+                </div>
+               
+            </div>
+        </div>
+    );
 }
 
-export default SignIn
+export default SignOut;
