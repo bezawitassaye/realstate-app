ceec9de57da520a79a2f0757eeb924cf2f04f558
bezawitassaye <beza1221love@gmail.com>
2024-06-25 21:58:43 +0300

    redux persist

--- client/redux/Stire.js
+++ client/redux/Stire.js
@@ -1,8 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from "./user/userSlice"
+// Store.js
+
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from "./user/userSlice";
+import { persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+import { persistStore } from 'redux-persist';
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    // other reducers if any
+});
+
+const persistConfig = {
+    key: "root",
+    storage,
+    version: 1,
+    // Optionally add whitelist or blacklist for persisting specific reducers
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user:userReducer
-  },
-})
+    reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,16 @@
 
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {store} from '../redux/Stire.js'
-import {Provider} from "react-redux"
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+import { store, persistor } from '../redux/Stire'; // Correct path to Store.js
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+             <App />
+    </PersistGate>
+ 
   </Provider >,
 )
